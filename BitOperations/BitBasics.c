831f75e258e2b1080da8258f4fc65dbc018c60be
Bertrand <bertrand.bak@proton.me>
2026-07-08 20:34:30 +0200

    two more excercises on masks

--- BitOperations/BitBasics.c
+++ BitOperations/BitBasics.c
@@ -6,6 +6,8 @@ uint8_t setbit(uint8_t value, uint8_t bit);
 uint8_t clearbit(uint8_t value, uint8_t bit);
 uint8_t togglebit(uint8_t value, uint8_t bit);
 uint8_t checkbit(uint8_t value, uint8_t bit);
+uint8_t setmask(uint8_t mask1, uint8_t mask2, uint8_t mask3);
+
 void printbinary(uint8_t value);
 
 int main(){
@@ -19,6 +21,20 @@ int main(){
     printbinary(value);
     uint8_t is_bit_set = checkbit(value,7);
     printf("%u \n",is_bit_set);
+    uint8_t maskof3 = setmask((1<<6),(1<<2),(1<<3));
+    printbinary(maskof3);
+    value |= maskof3;
+    printbinary(value);
+    uint8_t clearmask = setmask((1<<6),(1<<2),(1<<3));
+    value &= ~clearmask;
+    printbinary(value);
+
+    uint8_t MixMask = ( (1<<0) | (1<<7));
+    value |= MixMask;
+    uint8_t MixMask2 = ~(1<<7);
+    value &= MixMask2;
+    value ^= (1<<0);
+    printbinary(value);
     return 0;
 }
 
@@ -48,3 +64,7 @@ uint8_t togglebit(uint8_t value, uint8_t bit){
 uint8_t checkbit(uint8_t value, uint8_t bit){
     return (value&(1<<bit));
 }
+uint8_t setmask(uint8_t mask1, uint8_t mask2, uint8_t mask3){
+    return (mask1 | mask2 | mask3);
+}
+
